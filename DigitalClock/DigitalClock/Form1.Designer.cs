9edcb18474961dbfc72d68adb7617264d59178cd
Stanislav Nikolov <stanislavnikolov567@gmail.com>
2022-10-05 12:25:32 +0300

    design

--- DigitalClock/DigitalClock/Form1.Designer.cs
+++ DigitalClock/DigitalClock/Form1.Designer.cs
@@ -29,13 +29,80 @@ namespace DigitalClock
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblSecond = new System.Windows.Forms.Label();
+            this.lblDay = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTime.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblTime.Location = new System.Drawing.Point(68, 48);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(283, 128);
+            this.lblTime.TabIndex = 0;
+            this.lblTime.Text = "22:22";
+            // 
+            // lblSecond
+            // 
+            this.lblSecond.AutoSize = true;
+            this.lblSecond.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblSecond.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblSecond.Location = new System.Drawing.Point(340, 99);
+            this.lblSecond.Name = "lblSecond";
+            this.lblSecond.Size = new System.Drawing.Size(62, 50);
+            this.lblSecond.TabIndex = 1;
+            this.lblSecond.Text = "22\r\n";
+            // 
+            // lblDay
+            // 
+            this.lblDay.AutoSize = true;
+            this.lblDay.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblDay.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblDay.Location = new System.Drawing.Point(411, 279);
+            this.lblDay.Name = "lblDay";
+            this.lblDay.Size = new System.Drawing.Size(200, 65);
+            this.lblDay.TabIndex = 2;
+            this.lblDay.Text = "Tuesday";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblDate.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblDate.Location = new System.Drawing.Point(27, 269);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(356, 65);
+            this.lblDate.TabIndex = 3;
+            this.lblDate.Text = "October 4 2022";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(640, 398);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblDay);
+            this.Controls.Add(this.lblSecond);
+            this.Controls.Add(this.lblTime);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblSecond;
+        private System.Windows.Forms.Label lblDay;
+        private System.Windows.Forms.Label lblDate;
     }
 }
 
